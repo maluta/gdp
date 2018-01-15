0a9c9ac157fe89fe148b3db589bf9029c8b4c39c
Tiago Maluta <tiago.maluta@gmail.com>
2018-01-15 22:16:43 +0000

    google custom search

--- gdp/src/App.js
+++ gdp/src/App.js
@@ -3,6 +3,26 @@ import SearchBox from './components/search_box';
 import ResultsBox from './components/results_box';
 import './App.css';
 
+
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+  key: API_KEY,
+  cx : CX,
+});
+
+
+googleSearch.build({
+  q: "frações",
+  start: 5,
+  gl: "pt-BR", //geolocation, 
+  lr: "lang_pt",
+  num: 10 // Number of search results to return between 1 and 10, inclusive 
+}, function(error, response) {
+  console.log(response);
+});
+
+//-------------------------
+
 class App extends Component {
   render() {
     return (
@@ -10,10 +30,8 @@ class App extends Component {
         <header className="App-header">
         </header>
         <h1>Busca do Professor</h1>
-        <p className="App-intro">
         <SearchBox />
         <ResultsBox />
-        </p>
       </div>
     );
   }
