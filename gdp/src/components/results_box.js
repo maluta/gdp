86abd48aa15edab895d7d19ed1e89b390a5afe55
Tiago Maluta <tiago.maluta@gmail.com>
2018-01-15 23:12:11 +0000

    dummy
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- gdp/src/components/results_box.js
+++ gdp/src/components/results_box.js
@@ -4,11 +4,14 @@ class ResultsBox extends Component {
     constructor(props) {
       super(props);
       //const r = props.results.items.map( (re) => { return <li>{re}</li> });
+      console.log("ResultsBox");
+
    }
 
     render() { 
          return (
          <ul className="col-md-4 list-group">
+		<li>RESULTS HERE</li>
          </ul>
          );
 
